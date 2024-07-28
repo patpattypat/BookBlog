0f08c90c004bb0bbac1d72b970cbceb6d14263d8
patpattypat <stoll.p@hotmail.com>
2024-07-28 12:39:48 +0200

    react typescript setup

---
+++ static/book-blog/src/containers/index.ts
@@ -0,0 +1,7 @@
+export {Home} from './home';
+export {AboutPage} from './about-page';
+export { BlogPage } from './blog-page';
+export { StoryPage } from './story-page';
+export { Cards } from './cards';
+export { CurrentEvents } from './current-events';
+export { Library } from './library';
--- static/src/containers/home/home.tsx
+++
@@ -1,5 +0,0 @@
-export const Home: React.FC = () = {
-	return (
-		<div className="home">Home</div>
-	);
-}
