20be695401806c0d70d35bc3fa7b6dfc9a7acf19
patpattypat <stoll.p@hotmail.com>
2024-08-01 17:05:36 +0200

    wip:home screen

--- static/book-blog/src/containers/home/home.tsx
+++ static/book-blog/src/containers/home/home.tsx
@@ -8,6 +8,40 @@ export const Home: React.FC = () => {
 			<Header />
 			<div className="content" >
 				Home
+				 <div className="grid-container">
+				 	<div className="grid-item">
+				  		<img src="home-pictures/small-1.jpg" alt="small img"/>
+						<span className="text about">About me</span>
+				  	</div>
+				 	<div className="grid-item">
+				   		<img src="home-pictures/small-2.jpg" alt="small img 2" />
+					</div>
+				  	<div className="grid-item vertical">
+					 	<img src="home-pictures/vertical-1.jpg" alt="vertical img 1" />
+					<span className="text aktuelles">Aktuelles</span>
+					</div>
+				  	<div className="grid-item big">
+					 	<img src="home-pictures/big-1.jpg" alt="big img 1" />
+					<span className="text stories">Kurzgeschichten</span>
+					</div>
+				  	<div className="grid-item">
+					 	<img src="home-pictures/small-3.jpg" alt="small img 3" />
+					</div>
+				  	<div className="grid-item">
+					 	<img src="home-pictures/small-4.jpg" alt="small img 4" />
+					</div>
+				  	<div className="grid-item horizontal">
+					 	<img src="home-pictures/horizontal-1.jpg" alt="horizontal img 1" />
+					<span className="text blog">Blog</span>
+					</div>
+				  	<div className="grid-item">
+					 	<img src="home-pictures/small-5.jpg" alt="small img 5" />
+					<span className="text cards">Karten</span>
+					</div>
+				  	<div className="grid-item horizontal">
+					 	<img src="home-pictures/horizontal-2.jpg" alt="horizontal img 2" />
+					</div>
+				</div> 
 			</div >
 		</div>
 	);
