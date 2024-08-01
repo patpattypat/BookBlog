3dc317df7cbd6461cc256a92bb1588b3c9bbebce
patpattypat <stoll.p@hotmail.com>
2024-08-01 14:22:57 +0200

    finish header web view

--- static/book-blog/src/containers/header/header.tsx
+++ static/book-blog/src/containers/header/header.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link} from 'react-router-dom';
 import './header.scss';
+import { ReactComponent as Heaader } from './assets/header.svg';
 
 export const Header: React.FC = () => {
 	return (
 		<div className="header">
 			<div className="logo">
-				Logo
+				<Heaader />
 			</div>
 			<div className="menu">
 				<Link className="btn btn-menu" to="/about">About</Link>
